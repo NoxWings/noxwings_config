6a72cea0732c8075385b76a16d06197f7b901a8c
NoxWings <david@gamevy.com>
2018-08-28 10:55:45 +0200

    glog alias

--- dotfiles/aliases.zsh
+++ dotfiles/aliases.zsh
@@ -16,6 +16,7 @@ alias gfr='git fetch && git rebase origin/master'
 alias gsfr='git stash -u && gfr && git stash pop'
 alias gdbCandidates='git branch --merged | egrep -v "(^\*|master|dev)"'
 alias gdbAll='gdbCandidates | xargs git branch -D'
+alias glog='git log --graph --abbrev-commit --decorate --format=format:"%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)"'
 
 
 # Testing aliases
