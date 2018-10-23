40227e431a40c444f3bb77acb0282d709344313a
NoxWings <david@gamevy.com>
2018-10-23 12:39:35 +0200

    glines alias

--- dotfiles/aliases.zsh
+++ dotfiles/aliases.zsh
@@ -18,6 +18,7 @@ alias gsfr='git stash -u && gfr && git stash pop'
 alias gdbCandidates='git branch --merged | egrep -v "(^\*|master|dev)"'
 alias gdbAll='gdbCandidates | xargs git branch -D'
 alias glog='git log --graph --abbrev-commit --decorate --format=format:"%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)"'
+alias glines='echo `git diff master --shortstat`'
 
 # Testing aliases
 function unit-test {
