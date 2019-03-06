a08519a58d5831d42cb989de6fdd48c026535334
David (NoxWings) García Miguel <noxwings@gmail.com>
2019-03-06 10:21:58 +0100

    add new git alias

--- dotfiles/aliases.zsh
+++ dotfiles/aliases.zsh
@@ -19,6 +19,7 @@ alias gdbCandidates='git branch --merged | egrep -v "(^\*|master|dev)"'
 alias gdbAll='gdbCandidates | xargs git branch -D'
 alias glog='git log --graph --abbrev-commit --decorate --format=format:"%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)"'
 alias glines='echo `git diff master --shortstat`'
+alias gcnv='git commit --no-verify'
 
 # Testing aliases
 function unit-test {
